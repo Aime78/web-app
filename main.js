f3a28320900c7e323eda568ac1684d85abb4a769
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2022-05-22 14:14:51 +0200

    font-end part

--- main.js
+++ main.js
@@ -1,92 +1,158 @@
 
 // The default product-type 
-const productSpecs = document.querySelector('.product-specs');
+const productSpecsData = document.querySelector('.product-specs');
 const productType = document.querySelector('#productType');
 
-const label = document.createElement('label');
-const input = document.createElement('input');
-const dvdSizeControl = document.createElement('span');
+const dvdSize = document.createElement('div');
+const labelDVD = document.createElement('label');
+const inputDVD = document.createElement('input');
+const productSpecControlDVD = document.createElement('span');
+const productSpecControlBook = document.createElement('span');
+const productSpecControlHeight = document.createElement('span');
+const productSpecControlWidth = document.createElement('span');
+const productSpecControlLength = document.createElement('span');
+
 
 if (productType.value == 'dvd') {
-    label.textContent = 'Size(MB)';
-    input.setAttribute('type','text');
-    input.setAttribute('placeholder','Size of the DVD-disc');
-    input.setAttribute('id','size');
+    labelDVD.textContent = 'Size(MB)';
+    inputDVD.setAttribute('type','text');
+    inputDVD.setAttribute('placeholder','Size of the DVD-disc');
+    inputDVD.setAttribute('id','size');
+    inputDVD.setAttribute('autocomplete','off');
+    dvdSize.setAttribute('class','dvd-size');
+    dvdSize.setAttribute('id','specs');
+    productSpecControlDVD.setAttribute('class','dvd-message');
 }
 
-productSpecs.appendChild(label);
-productSpecs.appendChild(input);
-productSpecs.appendChild(dvdSizeControl);
+dvdSize.appendChild(labelDVD);
+dvdSize.appendChild(inputDVD);
+dvdSize.appendChild(productSpecControlDVD);
+productSpecsData.append(dvdSize);
 
 // Change the product type
 productType.addEventListener('change', function(e){
 
+    const productSpecs = document.querySelectorAll('#specs');
     if (productType.value == 'book') {
-        const furnitureSpecs = document.querySelectorAll('.product-type');
-        if(furnitureSpecs.length != 0) {
-            furnitureSpecs[0].remove();
-            furnitureSpecs[1].remove();
+
+        if (productSpecs.length == 1) {
+            productSpecs[0].remove();
         }
-        label.textContent = 'Weight(KG)';
-        input.setAttribute('type','text');
-        input.setAttribute('placeholder','Weight of the book');
-        input.setAttribute('id','weight');
+        else {
+            productSpecs[0].remove();
+            productSpecs[1].remove();
+            productSpecs[2].remove();
+        }
+
+        const bookWeight = document.createElement('div');
+        const labelWeight = document.createElement('label');
+        const inputWeight= document.createElement('input');
+
+        labelWeight.textContent = 'Weight(KG)';
+        inputWeight.setAttribute('type','text');
+        inputWeight.setAttribute('placeholder','Weight of the book');
+        inputWeight.setAttribute('id','weight');
+        inputWeight.setAttribute('autocomplete','off');
+        bookWeight.setAttribute('class','book-weight');
+        bookWeight.setAttribute('id','specs');
+        productSpecControlBook.setAttribute('class','book-message');
+
+        bookWeight.appendChild(labelWeight);
+        bookWeight.appendChild(inputWeight);
+        bookWeight.appendChild(productSpecControlBook);
+        productSpecsData.appendChild(bookWeight);
+
+
     }
 
     else if (productType.value == 'furniture') {
-        const divHeight = document.createElement('div');
-        const labelLength = document.createElement('label');
-        const inputLength = document.createElement('input');
-        const divLength = document.createElement('div');
 
-        const labelWidth = document.createElement('label');
-        const inputWidth = document.createElement('input');
-        const divWidth = document.createElement('div');
+        productSpecs[0].remove();
 
-        label.textContent = 'Height(CM)';
-        input.setAttribute('type','text');
-        input.setAttribute('placeholder','Height of the furniture');
-        input.setAttribute('id','height');
-        //divHeight.setAttribute('class', 'product-input');
+        const furnitureHeight = document.createElement('div');
+        const heightLabel = document.createElement('label');
+        const heightInput = document.createElement('input');
 
-        labelWidth.textContent = 'Width(CM)';
-        inputWidth.setAttribute('type','text');
-        inputWidth.setAttribute('placeholder','Width of the furniture');
-        inputWidth.setAttribute('id','width');
-        divWidth.setAttribute('class', 'product-type');
-
-        labelLength.textContent = 'Length(CM)';
-        inputLength.setAttribute('type','text');
-        inputLength.setAttribute('placeholder','Length of the furniture');
-        inputLength.setAttribute('id','length');
-        divLength.setAttribute('class', 'product-type');
-
-        divHeight.appendChild(label);
-        divHeight.appendChild(input);
-        productSpecs.appendChild(divHeight);
-
-        divWidth.appendChild(labelWidth);
-        divWidth.appendChild(inputWidth);
-        productSpecs.appendChild(divWidth);
+        const furnitureLength = document.createElement('div');
+        const lengthLabel = document.createElement('label');
+        const lengthInput = document.createElement('input');
         
-        divLength.appendChild(labelLength);
-        divLength.appendChild(inputLength);
-        productSpecs.appendChild(divLength);
+
+        const furnitureWidth = document.createElement('div');
+        const widthLabel = document.createElement('label');
+        const widthInput = document.createElement('input');
+
+        heightLabel.textContent = 'Height(CM)';
+        heightInput.setAttribute('type','text');
+        heightInput.setAttribute('placeholder','Height of the furniture');
+        heightInput.setAttribute('id','height');
+        heightInput.setAttribute('autocomplete','off');
+        productSpecControlHeight.setAttribute('class','furniture-message');
+
+        furnitureHeight.setAttribute('class', 'furniture-height');
+        furnitureHeight.setAttribute('id', 'specs');
+
+        widthLabel.textContent = 'Width(CM)';
+        widthInput.setAttribute('type','text');
+        widthInput.setAttribute('placeholder','Width of the furniture');
+        widthInput.setAttribute('id','width');
+        widthInput.setAttribute('autocomplete','off');
+        productSpecControlWidth.setAttribute('class','furniture-message');
+       
+        furnitureWidth.setAttribute('class', 'furniture-width');
+        furnitureWidth.setAttribute('id', 'specs');
+
+        lengthLabel.textContent = 'Length(CM)';
+        lengthInput.setAttribute('type','text');
+        lengthInput.setAttribute('placeholder','Length of the furniture');
+        lengthInput.setAttribute('id','length');
+        lengthInput.setAttribute('autocomplete','off');
+        productSpecControlLength.setAttribute('class','furniture-message');
+        
+        furnitureLength.setAttribute('class', 'furniture-length');
+        furnitureLength.setAttribute('id', 'specs');
+
+        furnitureHeight.appendChild(heightLabel);
+        furnitureHeight.appendChild(heightInput);
+        furnitureHeight.appendChild(productSpecControlHeight);
+        productSpecsData.appendChild(furnitureHeight);
+
+        furnitureWidth.appendChild(widthLabel);
+        furnitureWidth.appendChild(widthInput);
+        furnitureWidth.appendChild(productSpecControlWidth);
+        productSpecsData.appendChild(furnitureWidth);
+
+        furnitureLength.appendChild(lengthLabel);
+        furnitureLength.appendChild(lengthInput);
+        furnitureLength.appendChild(productSpecControlLength);
+        productSpecsData.appendChild(furnitureLength);
         
     }
     else {
-        const furnitureSpecs = document.querySelectorAll('.product-type');
-        if(furnitureSpecs.length != 0) {
-            furnitureSpecs[0].remove();
-            furnitureSpecs[1].remove();
+        
+        if (productSpecs.length == 1) {
+            productSpecs[0].remove();
         }
-        label.textContent = 'Size(MB)';
-        input.setAttribute('type','text');
-        input.setAttribute('placeholder','Size of the DVD-disc');
-        input.setAttribute('id','size');
+        else {
+            productSpecs[0].remove();
+            productSpecs[1].remove();
+            productSpecs[2].remove();
+        }
+
+        labelDVD.textContent = 'Size(MB)';
+        inputDVD.setAttribute('type','text');
+        inputDVD.setAttribute('placeholder','Size of the DVD-disc');
+        inputDVD.setAttribute('id','size');
+        dvdSize.setAttribute('class','dvd-size');
+        dvdSize.setAttribute('id','specs');
+
+        dvdSize.appendChild(labelDVD);
+        dvdSize.appendChild(inputDVD);
+        dvdSize.appendChild(productSpecControlDVD);
+        productSpecsData.append(dvdSize);        
 
     }
-})
+});
 
 // Check that empty fields are submitted 
 const inputs = Array.from(document.querySelectorAll('input'));
@@ -98,28 +164,69 @@ form.addEventListener('submit', function(e) {
     const skuControl = document.querySelector('.sku-control');
     const nameControl = document.querySelector('.name-control');
     const priceControl = document.querySelector('.price-control');
+    const messageQuit = Array.from(document.querySelectorAll('span'));
+    messageQuit.forEach(message => message.textContent = '');
 
+    const productSpecs = document.querySelectorAll('#specs');
+    const productSpecsInput = document.querySelectorAll('#specs > input');
+    console.log(productSpecsInput[0]);
+    e.preventDefault();
+
+    // Check that all the fields are not empty
+    if(inputs[0].value == '' && inputs[1].value == '' && inputs[2].value == '' && inputs[3].value == '') {
+        errorMessage.textContent = 'Please submit all required data';
+        skuControl.textContent = 'Please provide the sku of the product';
+        nameControl.textContent = 'Please provide the name of the product';
+        priceControl.textContent = 'Please provide the price of the product';
+    }
+
+    if (productSpecs.length == 1) {
+        if (productSpecsInput[0].value == '' && productSpecsInput[0].getAttribute('id') == 'size') {
+            productSpecControlDVD.textContent = 'Please provide the size of the DVD-disc';
+            e.preventDefault();
+        }
+        else if (productSpecsInput[0].value == '' && productSpecsInput[0].getAttribute('id') == 'weight'){
+            productSpecControlBook.textContent = 'Please provide the weight of the Book';
+            e.preventDefault();
+        }
+    }
+    else {
+        
+        if (productSpecsInput[0].value == '' && productSpecsInput[0].getAttribute('id') == 'height') {
+            productSpecControlHeight.textContent = 'Please provide the height of the furniture';
+            e.preventDefault();
+        }
+        if (productSpecsInput[1].value == '' && productSpecsInput[1].getAttribute('id') == 'width') {
+            productSpecControlWidth.textContent = 'Please provide the width of the furniture';
+            e.preventDefault();
+        }
+        if (productSpecsInput[2].value == '' && productSpecsInput[2].getAttribute('id') == 'length') {
+            productSpecControlLength.textContent = 'Please provide the length of the furniture';
+            e.preventDefault();
+        }
+    } 
+    
+    // Check that a particular field is not empty
     if(inputs[0].value == '') {
         errorMessage.textContent = 'Please submit all required data';
         skuControl.textContent = 'Please provide the sku of the product';
         e.preventDefault();
     }
-    else if(inputs[1].value == '') {
+    if(inputs[1].value == '') {
         errorMessage.textContent = 'Please submit all required data';
         nameControl.textContent = 'Please provide the name of the product';
         e.preventDefault();
     }
-    else if(inputs[2].value == '') {
+    if(inputs[2].value == '') {
         errorMessage.textContent = 'Please submit all required data';
         priceControl.textContent = 'Please provide the price of the product';
         e.preventDefault();
     }
-    else if(inputs[3].value == '') {
+    if(inputs[3].value == '') {
         errorMessage.textContent = 'Please submit all required data';
-        dvdSizeControl.textContent = 'Please provide the size of the DVD-disc';
+        productSpecControlDVD.textContent = 'Please provide the size of the DVD-disc';
         e.preventDefault();
     }
-
     
 });
 
